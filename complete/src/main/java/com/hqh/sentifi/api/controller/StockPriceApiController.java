85c31bd74da33e22a26506b778779d671693a29e
Ho Quoc Huy <h.ho@laudert.de>
2018-11-23 18:43:15 +0700

    Add 1st APi

--- complete/src/main/java/com/hqh/sentifi/api/controller/StockPriceApiController.java
+++ complete/src/main/java/com/hqh/sentifi/api/controller/StockPriceApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
@@ -22,15 +23,22 @@ public class StockPriceApiController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/{tickerSymbol}/{actionName}", method = RequestMethod.GET)
-    public Greeting sayHello(
-        @RequestParam(value = "startDate") @DateTimeFormat(pattern = DATE_PARTERN) Date startDate,
-        @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = DATE_PARTERN) Date endDate,
-        @PathVariable("actionName") String actionName,
-        @PathVariable("tickerSymbol") String tickerSymbol) {
+    public Greeting sayHello(@RequestParam(value = "startDate") @DateTimeFormat(pattern = DATE_PARTERN) Date startDate,
+                             @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = DATE_PARTERN) Date endDate,
+                             @PathVariable("actionName") String actionName,
+                             @PathVariable("tickerSymbol") String tickerSymbol) {
         System.out.println(startDate);
         System.out.println(endDate);
         System.out.println(actionName);
         System.out.println(tickerSymbol);
+
+        final String uri = "http://localhost:8080/springrestexample/employees.xml";
+
+        final RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        System.out.println(result);
+
         return new Greeting(counter.incrementAndGet(), "sdsd");
     }
 }
